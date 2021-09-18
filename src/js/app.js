543e81fd2254c03a0fd9625cd7dd7f04d5f9eeaa
Emo <emotupev11@gmail.com>
2021-09-18 11:33:59 +0300

    fix: dom manipulation only on interact with the body element

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,15 @@
-import '../scss/app.scss';
+// import '../scss/app.scss';
 
 window.addEventListener('DOMContentLoaded', () => {
 	// This block will be executed once the page is loaded and ready
 
-	for (let i = 0; i < 5; i++) {
-		const article = document.createElement('article');
-		article.classList.add('message');
-		article.textContent = `This is article ${i} text content`;
-		document.body.appendChild(article);
-	}
+	document.body.addEventListener('click', () => {
+		for (let i = 0; i < 5; i++) {
+			console.log('setaa');
+			const article = document.createElement('article');
+			article.classList.add('message');
+			article.textContent = `This is article ${i} text content`;
+			document.body.appendChild(article);
+		}
+	});
 });
